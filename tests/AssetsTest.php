03387e5b1cb758d804b0f22b5304abdca06bc7fa
yoeunes <younes.khoubza@gmail.com>
2017-05-06 21:29:29 +0100

    test other functions

--- tests/AssetsTest.php
+++ tests/AssetsTest.php
@@ -58,4 +58,61 @@ class AssetsTest extends TestCase
         $this->assertEquals('<link rel="stylesheet" type="text/css" href="fake_fontawesome.css">', fontawesome(null, 'fake_fontawesome.css'));
         $this->assertEquals('<link rel="stylesheet" type="text/css" href="fake_fontawesome.css">', fontawesome('3.0.0', 'fake_fontawesome.css'));
     }
+
+    /** @test */
+    public function it_render_jquery_link()
+    {
+        $this->assertEquals('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>', jquery());
+    }
+
+    /** @test */
+    public function it_render_jquery_with_another_version()
+    {
+        $this->assertEquals('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>', jquery('2.0.0'));
+    }
+
+    /** @test */
+    public function it_render_jquery_local_file()
+    {
+        $this->assertEquals('<script type="text/javascript" src="jquery.min.js"></script>', jquery(null, 'jquery.min.js'));
+        $this->assertEquals('<script type="text/javascript" src="jquery.min.js"></script>', jquery('2.0.0', 'jquery.min.js'));
+    }
+
+    /** @test */
+    public function it_render_bootstrap_css()
+    {
+        $this->assertEquals('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">', bootstrap_css());
+    }
+
+    /** @test */
+    public function it_render_bootstrap_with_another_version()
+    {
+        $this->assertEquals('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/css/bootstrap.min.css">', bootstrap_css('3.0.0'));
+    }
+
+    /** @test */
+    public function it_render_bootstrap_local_file()
+    {
+        $this->assertEquals('<link rel="stylesheet" type="text/css" href="bootstrap.min.js">', bootstrap_css(null, 'bootstrap.min.js'));
+        $this->assertEquals('<link rel="stylesheet" type="text/css" href="bootstrap.min.js">', bootstrap_css('3.0.0', 'bootstrap.min.js'));
+    }
+
+    /** @test */
+    public function it_render_bootstrap_js()
+    {
+        $this->assertEquals('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>', bootstrap_js());
+    }
+
+    /** @test */
+    public function it_render_specific_version_of_bootrap()
+    {
+        $this->assertEquals('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/js/bootstrap.min.js"></script>', bootstrap_js('3.0.0'));
+    }
+
+    /** @test */
+    public function it_render_bootsrap_local_js_file()
+    {
+        $this->assertEquals('<script type="text/javascript" src="bootstrap.min.js"></script>', bootstrap_js(null, 'bootstrap.min.js'));
+        $this->assertEquals('<script type="text/javascript" src="bootstrap.min.js"></script>', bootstrap_js('3.0.0', 'bootstrap.min.js'));
+    }
 }
